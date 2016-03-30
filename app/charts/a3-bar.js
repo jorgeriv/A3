2ecf8160a4c460971e448aa969619bc4cc121168
Jorge Rivera <jorgeriv@gmail.com>
2016-03-30 14:23:40 -0500

    use a3 angular module instead of a3 global variable

--- app/charts/a3-bar.js
+++ app/charts/a3-bar.js
@@ -1,19 +1,20 @@
-'use strict';
-
-a3.visualization('bar', function(svg, scope){
+;(function(){'use strict';
+angular.module('a3')
+	.visualization('bar', function(svg, scope){
 	var width = scope.innerWidth;
 	var height = scope.innerHeight;
 	var barWidth = width/scope.data.length;
 	var y = d3.scale.linear()
-	    .range([height, 0])
-	    .domain([0, d3.max(scope.data, function(d) { return d; })]);
+	.range([height, 0])
+	.domain([0, d3.max(scope.data, function(d) { return d; })]);
 	var bar = svg.selectAll('g')
-				.data(scope.data)
-				.enter().append('g');
+	.data(scope.data)
+	.enter().append('g');
 
-		bar.append('rect')
-			  .attr('x', function(d, i) {return i * barWidth; })
-		      .attr('y', function(d) {return y(d);})
-		      .attr('height', function(d) { return height - y(d); })
-		      .attr('width', function(){ return  barWidth - 1;});
+	bar.append('rect')
+	.attr('x', function(d, i) {return i * barWidth; })
+	.attr('y', function(d) {return y(d);})
+	.attr('height', function(d) { return height - y(d); })
+	.attr('width', function(){ return  barWidth - 1;});
 });
+}());
--- app/charts/a3-line.js
+++ app/charts/a3-line.js
@@ -1,30 +1,30 @@
-'use strict';
-
+;(function(){'use strict';
+var a3 = angular.module('a3');
 a3.visualization('line', function(svg, scope){
 	var width = scope.innerWidth;
 	var height = scope.innerHeight;
 	var x = d3.scale.linear()
-		.domain([0, scope.data.length])
-		.range([0, width]);
+	.domain([0, scope.data.length])
+	.range([0, width]);
 	var y = d3.scale.linear()
-		.domain([
-			0,
-			d3.max(scope.data, function(d){return d;})
-		])
-		.range([height, 0]);
+	.domain([
+		0,
+		d3.max(scope.data, function(d){return d;})
+	])
+	.range([height, 0]);
 	var line =  d3.svg.line()
-				.x(function(d, i){ return x(i); })
-				.y(function(d){ return y(d); })
-				.interpolate('linear');
+	.x(function(d, i){ return x(i); })
+	.y(function(d){ return y(d); })
+	.interpolate('linear');
 	svg = svg.append('g')
 	.append('path')
-
-  .attr('d', line(scope.data))
-  .attr('stroke', 'red')
-  .attr('stroke-width', 1)
-  .attr('fill', 'none');
+	.attr('d', line(scope.data))
+	.attr('stroke', 'red')
+	.attr('stroke-width', 1)
+	.attr('fill', 'none');
 
 	//.datum(scope.data)
 	//.attr('class', 'line')
 	//.attr('d', line);
 });
+}());
